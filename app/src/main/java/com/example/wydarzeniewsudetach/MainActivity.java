ddb9261ab77d97d1699dad878a8611d9e97e488a
Dawid Grzybowski <9572736u@qed.local>
2024-12-06 11:26:29 +0100

    Initial commit

---
+++ app/src/main/java/com/example/wydarzeniewsudetach/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.wydarzeniewsudetach;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity{
+    private EditText email;
+    private TextView licznik;
+    private Button button;
+    private Button button2;
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+    }
